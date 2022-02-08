b4795a598a91ec0997c3cfe71fe8cc61bbb92935
HebiSunrise <93012812+HebiSunrise@users.noreply.github.com>
2022-02-08 18:37:32 +0300

    fix: second solution

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@
 
 module.exports = function towelSort (matrix) {
     let arr = [];
+    let lr = true
     for (let i = 0; i<matrix.length; i++){
         let t = matrix[i];
-        for (let j = 0; j<r.length; j++){
-            arr.push(t[j]);
+        if (lr){
+            for (let j = 0; j<r.length; j++){
+                arr.push(t[j]);
+            }
+            lr =false;
+            continue
+        }
+        if (!lr){
+            for (let j = r.length-1; j>=0; j--){
+                arr.push(t[j]);
+            }
+            lr =true;
         }
     }
     return arr
